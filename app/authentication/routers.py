d6ddc79f37f050b546c9cacdcb073b1d2c487771
marlonmartins2 <marlon.azevedo.m@gmail.com>
2024-03-22 11:03:54 -0300

    change: change token_data

--- app/authentication/routers.py
+++ app/authentication/routers.py
@@ -50,7 +50,8 @@ async def login(data: SignInModel):
     
     token_data = {
         "user_id": user["id"],
-        "username": user["username"],
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
         "email": user["email"],
         "role_id": user["role_id"],
     }
